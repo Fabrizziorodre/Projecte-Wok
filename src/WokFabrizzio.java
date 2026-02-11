5c221dc35083830aa704e33fa08c0dc885106f58
Fabrizziorodre <fabrizzior2224@gmail.com>
2026-02-11 16:28:45 +0100

    Add files via upload

---
+++ src/WokFabrizzio.java
@@ -0,0 +1,93 @@
+package src;
+
+import src.wokmodel.*;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static src.UtilsFabrizzio.Utils.llegirInt;
+import static src.UtilsWok.*;
+
+public class WokFabrizzio {
+
+    public static void main(String[] args) {
+        int  mida, salsa;
+        double preu;
+        ArrayList<Integer> ingredients;
+        Base base = escollirBase();
+        ingredients = escollirIngredients();
+        salsa = escollirSalsa();
+        //TODO: Traslladar a dins de Wok preu = calcularPreu(base, mida, ingredients, salsa);
+        //TODO: mostrarResumComanda(wok);
+    }
+
+    private static void mostrarResumComanda(int base, int mida, ArrayList<Integer> ingredients, int salsa, double preu) {
+        System.out.println("\nResum de la comanda:");
+        System.out.println("Base: " + basesDesc[base] + " - " + basePreus[base] + "€");
+        System.out.println("Mida: " + (mida == 1 ? "Petita" : "Gran (+1.50€)"));
+        System.out.println("Ingredients: ");
+        for (int ing : ingredients) {
+            System.out.println(" - " + ingredientsDesc[ing] + " - " + preusIngredients[ing] + "€");
+        }
+        System.out.println("Salsa: " + salses[salsa]);
+        System.out.println("Preu total: " + String.format("%.2f", preu) + "€");
+    }
+
+    private static double calcularPreu(int base, int mida, ArrayList<Integer> ingredients, int salsa) {
+        double preu = basePreus[base];
+        if (mida == 2) preu += 1.50;
+        for (int ing : ingredients) {
+            preu += preusIngredients[ing];
+        }
+        return preu;
+    }
+
+    private static int escollirSalsa() {
+        System.out.println("Salses disponibles:");
+        for (int i = 0; i < salses.length; i++) {
+            System.out.println((i + 1) + " - " + salses[i]);
+        }
+        return llegirInt(scan, "Escolleix una salsa:", 1, salses.length) - 1;
+    }
+
+    private static ArrayList<Integer> escollirIngredients() {
+        ArrayList<Integer> ingredients = new ArrayList<>();
+        System.out.println("Ingredients disponibles:");
+        for (int i = 0; i < ingredientsDesc.length; i++) {
+            System.out.println((i + 1) + " - " + ingredientsDesc[i] + " - " + preusIngredients[i] + "€");
+        }
+        int opcio;
+        do {
+            opcio = llegirInt(scan, "Escolleix un ingredient (0 per acabar):", 0, ingredientsDesc.length);
+            if (opcio > 0) ingredients.add(opcio - 1);
+        } while (opcio != 0);
+        return ingredients;
+    }
+
+    private static String escollirMida(int base) {
+        if (baseMida[base]) {
+            System.out.println("Tria la mida del Wok:");
+            System.out.println("1 - Petita");
+            System.out.println("2 - Gran (+1.50€)");
+            if( llegirInt(scan, "Escolleix una mida:", 1, 2) ==1){
+                return "Petita";
+            }
+            else{
+                return "Gran";
+            }
+        } else {
+            System.out.println("Aquesta base només pot anar amb la mida gran del Wok.");
+            return "Gran";
+        }
+    }
+
+    private static Base escollirBase() {
+        System.out.println("Bases disponibles:");
+        for (int i = 0; i < basesDesc.length; i++) {
+            System.out.println((i + 1) + " - " + basesDesc[i] + " - " + basePreus[i] + "€");
+        }
+        int base = llegirInt(scan, "Escolleix una base:", 1, basesDesc.length) - 1;
+        String mida = escollirMida(base);
+        return new Base(basesDesc[base], mida,basePreus[base]);
+    }
+}
---
+++ src/wokmodel/Wok.java
@@ -0,0 +1,5 @@
+package src.wokmodel;
+
+public class Wok {
+
+}
